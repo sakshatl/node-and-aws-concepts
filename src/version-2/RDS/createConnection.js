505d1299c5300ae1e17965c79f3bc83aa336eca9
sakshatl <voteforsakshat@gmail.com>
2023-06-11 23:34:19 +0530

    Amazon AWS RDS setup and some useful methods using "pg" package

--- src/version-2/RDS/createConnection.js
+++ src/version-2/RDS/createConnection.js
@@ -1,15 +1,54 @@
-const mysql = require('mysql');
-require('dotenv').config({ path: '../../../.env' });
+import pg from 'pg';
 
-const connection = mysql.createConnection({
-  host: process.env.AWS_RDS_ENDPOINT, // AWS RDS endpoint,
-  user: process.env.AWS_RDS_USERNAME, // whatever is the database username
-  password: process.env.AWS_RDS_PASSWORD,
-  port: process.env.AWS_RDS_PORT
+const client = new pg.Client({
+  host: process.env.AWS_RDS_ENDPOINT,
+  port: process.env.AWS_RDS_PORT,
+  database: 'hello-world',
+  user: process.env.AWS_RDS_USERNAME,
+  password: process.env.AWS_RDS_PASSWORD
 });
 
-connection.connect(function(err) {
-  if(err) throw err;
+// ===== CONNECTING TO DATABASE =====
+await client.connect();
 
-  console.log('Connected to the DB!');
-});
+async function createConnection() {
+  try {
+    const result = await client.query('SELECT NOW()');
+    if(result) {
+      console.log("Success: Connected to the database!");
+    }
+  } catch (err) {
+    console.log("Error: Failed in conneting to the database!");
+  }
+}
+
+async function createDatabase() {
+  const QUERY = "CREATE DATABASE node_and_aws_sandbox";
+  try {
+    const result = await client.query(QUERY);
+    if(result) {
+      console.log(result, 'Success: New database created');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createTable() {
+  const QUERY = "CREATE TABLE USERS (ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, EMAIL TEXT NOT NULL)";
+  try {
+    const result = await client.query(QUERY);
+    if(result) {
+      console.log('Success: New table created!');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+await createConnection();
+// await createDatabase();
+// await createTable();
+
+// === END THE CONNECTION TO DATABASE ===
+// await client.end();
